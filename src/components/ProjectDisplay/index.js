00c2e831eb9c807147586ed77dea6b37a91e4be5
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-10-28 15:20:09 +0000

    @1.0.8

--- src/components/ProjectDisplay/index.js
+++ src/components/ProjectDisplay/index.js
@@ -5,7 +5,14 @@ const ProjectDisplay = props => {
   const {projectName, projectLink, projectImg} = item
 
   return (
-    <li className="project-box">
+    <li
+      className="project-box"
+      style={{
+        background: `linear-gradient(#222222d0,#111111d0),url(${projectImg})`,
+        backgroundSize: '100% 100%',
+        backgroundPosition: '50% 50%',
+      }}
+    >
       <img src={projectImg} alt="project" className="project-img" />
       <div className="text-container">
         <h1 className="project-head">{projectName}</h1>
