0d766e8317815c5af6f9852583a93151550aed70
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-10-28 06:39:22 +0000

    @1.0.7.2

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,7 +3,7 @@ import './index.css'
 const NotFound = () => (
   <div className="not-found-container">
     <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      src="https://cdn.pixabay.com/photo/2021/07/21/12/49/error-6482984_960_720.png"
       alt="not found"
       className="not-found-img"
     />
